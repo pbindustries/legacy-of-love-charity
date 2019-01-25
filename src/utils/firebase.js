462ea2caeaac996e9b4bf4ce20db8a5610753965
Philip Bankier <pbankier@miso3.com>
2019-01-25 15:00:29 -0500

    got it this time

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,7 +4,9 @@ const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+  projectId: "education-charity",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: "989607735589"
 };
 
 firebase.initializeApp(config);
