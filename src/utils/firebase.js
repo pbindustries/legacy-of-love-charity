1f66b78e56c3240eb84fe626825b4fa0bb3c1af4
Philip Bankier <pbankier@miso3.com>
2019-01-28 14:26:28 -0500

    minor fix

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,9 +6,7 @@ const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: "education-charity",
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: "989607735589"
 };
 
 firebase.initializeApp(config);
